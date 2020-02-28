5d98d7359e336d362db768f52040bb3107a0bdb8
Сучков Дмитрий Вадимович <Suchkov.D.Vad@sberbank.ru>
2020-02-28 14:32:13 +0300

    kek

---
+++ build/precache-manifest.ab2b45a71d831bdce17da8e8cfd606e4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70683b6c9aedd76f4e56504c5eb5aaf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4752264bcbbe0721eba8",
+    "url": "/static/css/main.b415a3ab.chunk.css"
+  },
+  {
+    "revision": "04a0a99bbe676a000183",
+    "url": "/static/js/2.b99f0a39.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/static/js/2.b99f0a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4752264bcbbe0721eba8",
+    "url": "/static/js/main.6f81c002.chunk.js"
+  },
+  {
+    "revision": "ff19db2481426e5e6ca9",
+    "url": "/static/js/runtime-main.ae78184e.js"
+  },
+  {
+    "revision": "8d36efeb3349af073647c0c37ec995f1",
+    "url": "/static/media/Gilroy-Heavy.8d36efeb.ttf"
+  },
+  {
+    "revision": "8b54e9f220995a04223c46015c850cac",
+    "url": "/static/media/arm.8b54e9f2.png"
+  },
+  {
+    "revision": "42a66b672efc8abc54afb0ba647f3511",
+    "url": "/static/media/arm2.42a66b67.png"
+  },
+  {
+    "revision": "ccf57901afe033a71a7acf9eacec5d07",
+    "url": "/static/media/bang.ccf57901.png"
+  },
+  {
+    "revision": "910736e8a1e94cf4825f0637c3f182e1",
+    "url": "/static/media/boss_glasses.910736e8.png"
+  },
+  {
+    "revision": "c0aa2d813f3a28f088573a30828ff045",
+    "url": "/static/media/godzilla.c0aa2d81.png"
+  },
+  {
+    "revision": "4f88fdb594e586f3e9140a82b730fa12",
+    "url": "/static/media/godzilla2.4f88fdb5.png"
+  },
+  {
+    "revision": "8723ad43db8d1cb87e014d4d8ba16bd1",
+    "url": "/static/media/hmm_emoji.8723ad43.png"
+  },
+  {
+    "revision": "2a8086e88b4fa04a13e4a3b3d5326687",
+    "url": "/static/media/leg.2a8086e8.png"
+  },
+  {
+    "revision": "b909c27ba8ad3d1eab41d55b921492c4",
+    "url": "/static/media/leg2.b909c27b.png"
+  },
+  {
+    "revision": "ab24b6160d869058ba85eeacb93c44b3",
+    "url": "/static/media/star.ab24b616.png"
+  }
+]);
